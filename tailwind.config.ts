82fc12c9d246c2a52e948e5ef0eecdd8ec3ac044
Alysia Alfi <alfi@domainesia.com>
2024-11-28 13:16:57 +0700

    feat: add custom keyframes and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,37 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+		'pink-a': '#ECC7EF',
+		'orange-a': "#FF9F0E"
+      },
+      keyframes: {
+        slideUpEnter: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(50px)",
+          },
+          "100%": {
+            opacity: "100",
+            transform: "translateY(0px)",
+          },
+        },
+		slideUpLeave: {
+			"0%": {
+			  opacity: "100",
+			  transform: "translateY(0)",
+			},
+			"100%": {
+			  opacity: "0",
+			  transform: "translateY(50px)",
+			},
+		  },
+      },
+      animation: {
+        slideUpEnter: "slideUpEnter .3s ease-in-out",
+		slideUpLeave: "slideUpLeave .3s ease-in-out",
       },
     },
   },
   plugins: [],
 } satisfies Config;
+  
