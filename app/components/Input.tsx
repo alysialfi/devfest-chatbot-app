0c4c8e5ad61d4931af50dd01cf8cbe8b35c77ff3
Alysia Alfi <alfi@domainesia.com>
2024-11-30 02:02:06 +0700

    feat: adjust Input so that it's cleaner

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,42 +1,19 @@
 "use client";
-import { useChat } from "ai/react";
 
-export default function Input() {
-  const {
-    messages = [],
-    input = "",
-    handleInputChange,
-    handleSubmit,
-    isLoading,
-  } = useChat({
-    api: "/api/chat",
-  });
+type InputProps = {
+	val: string;
+	setVal: (val: string) => void;
+	placeholder: string;
+}
 
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.length === 0 ? (
-        <div>Loading messages...</div>
-      ) : (
-        messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap">
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </div>
-        ))
-      )}
+export default function Input({val, setVal, placeholder}: InputProps) {
 
-      <form onSubmit={handleSubmit}>
-        <input
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Ask something..."
-          disabled={isLoading}
-          className="text-gray-700"
-        />
-        <button type="submit" disabled={isLoading}>
-          Send
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<input
+			value={val}
+			onChange={(e) => setVal(e.target.value)}
+			placeholder={placeholder}
+			className="py-2 px-4 rounded-md text-gray-700 w-full border border-gray-300"
+		/>
+	);
 }
