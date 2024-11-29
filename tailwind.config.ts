f2cd4ec1369da658f7e7531e7580359e7de4f484
Alysia Alfi <alfi@domainesia.com>
2024-11-30 01:56:26 +0700

    feat: remove keyframes & animations, add more colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,33 +12,8 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
 		'pink-a': '#ECC7EF',
-		'orange-a': "#FF9F0E"
-      },
-      keyframes: {
-        slideUpEnter: {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(50px)",
-          },
-          "100%": {
-            opacity: "100",
-            transform: "translateY(0px)",
-          },
-        },
-		slideUpLeave: {
-			"0%": {
-			  opacity: "100",
-			  transform: "translateY(0)",
-			},
-			"100%": {
-			  opacity: "0",
-			  transform: "translateY(50px)",
-			},
-		  },
-      },
-      animation: {
-        slideUpEnter: "slideUpEnter .3s ease-in-out",
-		slideUpLeave: "slideUpLeave .3s ease-in-out",
+		'orange-a': "#FF9F0E",
+		'cream-a': '#F3EAE2'
       },
     },
   },
